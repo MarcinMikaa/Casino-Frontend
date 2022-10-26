d002a6f18380b90ed14514154fa7c450bb153a0f
Marcin <marcin.mika123@gmail.com>
2022-10-26 16:59:51 +0200

    Front register-panel validation added

--- src/components/pages/RegisterPage/RegisterPage.js
+++ src/components/pages/RegisterPage/RegisterPage.js
@@ -1,84 +1,129 @@
 import "./RegisterPage.css";
-import React, { useState } from "react";
-import { Form, Button, FloatingLabel } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from "react";
+import { Form, Button, FloatingLabel, Row, Col } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 
 function RegisterPage() {
-  const [registerName, setRegisterName] = useState("");
-  const [registerSurname, setRegisterSurname] = useState("");
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [registerRepeatPassword, setRegisterRepeatPassword] = useState("");
-  const [registerAge, setRegisterAge] = useState("");
+  const navigate = useNavigate();
+  const [action, setAction] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm();
+  const password = useRef({});
+  password.current = watch("password", "");
 
-  const register = () => {
-    if (registerPassword === registerRepeatPassword) {
-      axios({
-        method: "POST",
-        data: {
-          name: registerName,
-          surname: registerSurname,
-          email: registerEmail,
-          password: registerPassword,
-          age: registerAge,
-        },
-        withCredentials: true,
-        url: "http://localhost:4000/register",
-      }).then((res) => {
-        console.log(res);
-      });
-    } else {
-      alert("Hasła różnią się!!");
-    }
+  const registerOptions = {
+    name: { required: "What's your name?" },
+
+    surname: { required: "What's your surname?" },
+
+    email: {
+      required: "Email address is required",
+      type: "email",
+    },
+
+    password: { required: "Password is required" },
+
+    repeatPassword: {
+      required: "Repeat your password",
+      validate: (value) => value === password.current || "The passwords don't match",
+    },
+
+    age: {
+      required: "Enter your age",
+      min: {
+        value: 18,
+        message: "You must be 18 to create an account",
+      },
+    },
   };
+
+  const onSubmit = (user) => {
+    axios({
+      method: "POST",
+      data: {
+        name: user.name,
+        surname: user.surname,
+        email: user.email,
+        password: user.password,
+        age: user.age,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/user",
+    }).then((res) => {
+      setAction(res.data.message);
+      if (res.data.message === "User Created") {
+        setAction("");
+        navigate("/login");
+      }
+    });
+  };
+  const handleError = (errors) => {};
 
   return (
     <div className="register-page">
-      <Form className="register-form">
-        <h1>CREATE AN ACCOUNT</h1>
-        <Form.Label>Name</Form.Label>
-        <FloatingLabel controlId="floatingInput" label="Your Name" className="mb-3">
-          <Form.Control type="text" placeholder="Enter name" onChange={(e) => setRegisterName(e.target.value)} />
-        </FloatingLabel>
+      <Form className="register-form" onSubmit={handleSubmit(onSubmit, handleError)}>
+        <h2>CREATE AN ACCOUNT</h2>
+        <br />
+        <Row className="g-2">
+          <Col md>
+            <FloatingLabel controlId="floatingInputName" label="Name" className="mb-3">
+              <Form.Control type="text" placeholder="Enter name" {...register("name", registerOptions.name)} />
+              <small className="text-error">{errors?.name && errors.name.message}</small>
+            </FloatingLabel>
+          </Col>
 
-        <Form.Label>Surname</Form.Label>
-        <FloatingLabel controlId="floatingInput" label="Your Surname" className="mb-3">
-          <Form.Control type="text" placeholder="Enter surname" onChange={(e) => setRegisterSurname(e.target.value)} />
+          <Col md>
+            <FloatingLabel controlId="floatingInputSurname" label="Surname" className="mb-3">
+              <Form.Control type="text" placeholder="Enter surname" {...register("surname", registerOptions.surname)} />
+              <small className="text-error">{errors?.surname && errors.surname.message}</small>
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <FloatingLabel controlId="floatingInputEmail" label="Email Address" className="mb-3">
+          <Form.Control type="email" placeholder="Enter email" {...register("email", registerOptions.email)} />
+          <small className="text-error">{errors?.email && errors.email.message}</small>
         </FloatingLabel>
+        <Row className="g-2">
+          <Col md>
+            <FloatingLabel controlId="floatingInputPassword" label="Password" className="mb-3">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                {...register("password", registerOptions.password)}
+              />
+              <small className="text-error">{errors?.password && errors.password.message}</small>
+            </FloatingLabel>
+          </Col>
 
-        <Form.Label>E-mail</Form.Label>
-        <FloatingLabel controlId="floatingInput" label="Your Email" className="mb-3">
-          <Form.Control type="email" placeholder="Enter email" onChange={(e) => setRegisterEmail(e.target.value)} />
+          <Col md>
+            <FloatingLabel controlId="floatingInputRepeatPassword" label="Repeat Password" className="mb-3">
+              <Form.Control
+                type="password"
+                placeholder="Repeat password"
+                {...register("repeatPassword", registerOptions.repeatPassword)}
+              />
+              <small className="text-error">{errors?.repeatPassword && errors.repeatPassword.message}</small>
+            </FloatingLabel>
+          </Col>
+        </Row>
+        <FloatingLabel controlId="floatingInputAge" label="Age" className="mb-3">
+          <Form.Control type="number" placeholder="Age" {...register("age", registerOptions.age)} />
+          <small className="text-error">{errors?.age && errors.age.message}</small>
         </FloatingLabel>
-
-        <Form.Label>Password</Form.Label>
-        <FloatingLabel controlId="floatingInput" label="Your Password" className="mb-3">
-          <Form.Control type="password" placeholder="Password" onChange={(e) => setRegisterPassword(e.target.value)} />
-        </FloatingLabel>
-
-        <Form.Label>Repeat Password</Form.Label>
-        <FloatingLabel controlId="floatingInput" label="Repeat Password" className="mb-3">
-          <Form.Control
-            type="password"
-            placeholder="Repeat password"
-            onChange={(e) => setRegisterRepeatPassword(e.target.value)}
-          />
-        </FloatingLabel>
-
-        <Form.Label>Age</Form.Label>
-        <FloatingLabel controlId="floatingInput" label="Your Age" className="mb-3">
-          <Form.Control type="number" placeholder="Age" onChange={(e) => setRegisterAge(e.target.value)} />
-        </FloatingLabel>
-
-        <Form.Group className="mb-1" controlId="formGroupSubmit">
-          <Button variant="primary" type="submit" onClick={register}>
-            Submit
+        <Form.Group className="mb-3 submit-group" controlId="formGroupSubmit">
+          {action === "User with given E-mail already exists" ? <p>User with given Email already exists</p> : <p> </p>}
+          <Button variant="primary" type="submit" className="register-button">
+            CONTINUE
           </Button>
         </Form.Group>
-
         <Form.Text className="text-muted">Already have an account? </Form.Text>
-        <Form.Text className="text-muted" as={Link} to="/login">
+        <Form.Text className="text-primary" as={Link} to="/login">
           <b>Login here</b>
         </Form.Text>
       </Form>
