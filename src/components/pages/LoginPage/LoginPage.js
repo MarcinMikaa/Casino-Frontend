0eff5efa826d5997cb98051e519b1f8ade48962d
Marcin <marcin.mika123@gmail.com>
2022-10-27 11:53:29 +0200

    Style merged

--- src/components/pages/LoginPage/LoginPage.js
+++ src/components/pages/LoginPage/LoginPage.js
@@ -1,16 +1,18 @@
 import "./LoginPage.css";
-import {useForm} from "react-hook-form"
-import { ErrorMessage } from '@hookform/error-message';
+import { useForm } from "react-hook-form";
 import { Form, Button, FloatingLabel, Alert } from "react-bootstrap";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const LoginPage = () => {
-
-  const {register, handleSubmit, formState: {errors}} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [message, setMessage] = useState("");
-  const [alertVisibility, setAlertVisibility] = useState(false)
+  const [alertVisibility, setAlertVisibility] = useState(false);
   const navigate = useNavigate();
 
   const loginOptions = {
@@ -18,7 +20,7 @@ const LoginPage = () => {
       required: "Email address is required",
       type: "email",
     },
-    password: { required: "Password is required" }
+    password: { required: "Password is required" },
   };
 
   const signIn = async (email, password) => {
@@ -26,51 +28,64 @@ const LoginPage = () => {
       method: "POST",
       data: {
         email: email,
-        password: password
+        password: password,
       },
       withCredentials: true,
       url: "http://localhost:4000/user/login",
     }).then((res) => {
-      if(res.data === "No User Exist"){
+      if (res.data === "No User Exist") {
         setMessage(res.data);
         setAlertVisibility(true);
-      }else{
-        navigate("/")
+      } else {
+        navigate("/");
       }
-
     });
   };
-  
+
   const handleError = (errors) => {};
 
   return (
-    <div className="login-panel">
-      <Form className="login-form" onSubmit={handleSubmit((data) => {
-        handleError();
-        signIn(data.email, data.password);
-      })}>
+    <div className="login-panel auth-page">
+      <Form
+        className="login-form auth-form"
+        onSubmit={handleSubmit((data) => {
+          handleError();
+          signIn(data.email, data.password);
+        })}
+      >
         <h3>Sign in</h3>
         <FloatingLabel controlId="floatingInput" label="Enter email" className="mb-3">
-          <Form.Control className="login-input" {...register("email", loginOptions.email)} type="email" placeholder=" "/>
-          <ErrorMessage errors={errors} name="email" />
+          <Form.Control
+            className="login-input"
+            {...register("email", loginOptions.email)}
+            type="email"
+            placeholder=" "
+          />
+          <small className="text-error">{errors?.email && errors.email.message}</small>
         </FloatingLabel>
         <FloatingLabel controlId="floatingPassword" label="Enter Password">
-          <Form.Control className="login-input" {...register("password", loginOptions.password)} type="password"  placeholder=" "/>
-          <ErrorMessage errors={errors} name="password" />
+          <Form.Control
+            className="login-input"
+            {...register("password", loginOptions.password)}
+            type="password"
+            placeholder=" "
+          />
+          <small className="text-error">{errors?.password && errors.password.message}</small>
         </FloatingLabel>
-        <Button type="submit" className="login-button">Sign in</Button>
+        <Button type="submit" className="login-button">
+          Sign in
+        </Button>
       </Form>
-      {alertVisibility ? 
+      {alertVisibility ? (
         <Alert className="alert-message">
           <p>{message}</p>
-          <Button onClick={() => (setAlertVisibility(false))}>
-            Close
-          </Button>
-        </Alert> :
+          <Button onClick={() => setAlertVisibility(false)}>Close</Button>
+        </Alert>
+      ) : (
         ""
-      }
+      )}
     </div>
   );
-}
+};
 
 export default LoginPage;
